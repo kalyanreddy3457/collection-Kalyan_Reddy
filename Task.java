04c52dd97c800e695e8650f1415a88cafc671fd2
kalyanreddy3457 <60334621+kalyanreddy3457@users.noreply.github.com>
2020-03-01 17:40:14 +0530

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+class Task
+{
+public static void main(String k[])
+{
+ArrayList<String> b= new ArrayList<String>( Arrays.asList("Alex","Ramu","Sanju","Raju","Ajay","Vijay","Marry","Bhanu","Pari","Chintu"));
+System.out.println(b);
+b.add("Rana");
+System.out.println(b);
+b.remove(5);
+System.out.println(b);
+}
+}
